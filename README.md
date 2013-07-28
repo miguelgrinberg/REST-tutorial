c08e42e0427c40ace362014087c39705efc62b77
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-07-28 11:57:04 -0700

    added alternative server implementation based on Flask-RESTful

--- README.md
+++ README.md
@@ -12,5 +12,6 @@ Setup
 - Install Python 2.7 and git.
 - Run `setup.sh` (Linux, OS X, Cygwin) or `setup.bat` (Windows)
 - Run `./rest-server.py` to start the server (on Windows use `flask\Scripts\python rest-server.py` instead)
+- Alternatively, run `./rest-server-v2.py` to start the Flask-RESTful version of the server.
 - Open `http://localhost:5000/index.html` on your web browser to run the client
 
---
+++ rest-server-v2.py
@@ -0,0 +1,109 @@
+#!flask/bin/python
+
+"""Alternative version of the ToDo RESTful server implemented using the
+Flask-RESTful extension."""
+
+from flask import Flask, jsonify, abort, request, make_response, url_for
+from flask.views import MethodView
+from flask.ext.restful import Api, Resource, reqparse, fields, marshal
+from flask.ext.httpauth import HTTPBasicAuth
+ 
+app = Flask(__name__, static_url_path = "")
+api = Api(app)
+auth = HTTPBasicAuth()
+ 
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+ 
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify( { 'message': 'Unauthorized access' } ), 403)
+    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
+    
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+ 
+task_fields = {
+    'title': fields.String,
+    'description': fields.String,
+    'done': fields.Boolean,
+    'uri': fields.Url('task')
+}
+
+class TaskListAPI(Resource):
+    decorators = [auth.login_required]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, required = True, help = 'No task title provided', location = 'json')
+        self.reqparse.add_argument('description', type = str, default = "", location = 'json')
+        super(TaskListAPI, self).__init__()
+        
+    def get(self):
+        return { 'tasks': map(lambda t: marshal(t, task_fields), tasks) }
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        task = {
+            'id': tasks[-1]['id'] + 1,
+            'title': args['title'],
+            'description': args['description'],
+            'done': False
+        }
+        tasks.append(task)
+        return { 'task': marshal(task, task_fields) }, 201
+
+class TaskAPI(Resource):
+    decorators = [auth.login_required]
+    
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type = str, location = 'json')
+        self.reqparse.add_argument('description', type = str, location = 'json')
+        self.reqparse.add_argument('done', type = bool, location = 'json')
+        super(TaskAPI, self).__init__()
+
+    def get(self, id):
+        task = filter(lambda t: t['id'] == id, tasks)
+        if len(task) == 0:
+            abort(404)
+        return { 'task': marshal(task[0], task_fields) }
+        
+    def put(self, id):
+        task = filter(lambda t: t['id'] == id, tasks)
+        if len(task) == 0:
+            abort(404)
+        task = task[0]
+        args = self.reqparse.parse_args()
+        task['title'] = args.get('title', task['title'])
+        task['description'] = args.get('description', task['description'])
+        task['done'] = args.get('done', task['done'])
+        return { 'task': marshal(task, task_fields) }
+
+    def delete(self, id):
+        task = filter(lambda t: t['id'] == id, tasks)
+        if len(task) == 0:
+            abort(404)
+        tasks.remove(task[0])
+        return { 'result': True }
+
+api.add_resource(TaskListAPI, '/todo/api/v1.0/tasks', endpoint = 'tasks')
+api.add_resource(TaskAPI, '/todo/api/v1.0/tasks/<int:id>', endpoint = 'task')
+    
+if __name__ == '__main__':
+    app.run(debug = True)
