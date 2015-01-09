4a4269b87f902adf704eb1ff345e80622d29a293
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-01-09 09:48:40 -0800

    Python 2 & 3 compliance

--- rest-server-v2.py
+++ rest-server-v2.py
@@ -3,41 +3,43 @@
 """Alternative version of the ToDo RESTful server implemented using the
 Flask-RESTful extension."""
 
-from flask import Flask, jsonify, abort, request, make_response, url_for
-from flask.views import MethodView
+from flask import Flask, jsonify, abort, make_response
 from flask.ext.restful import Api, Resource, reqparse, fields, marshal
 from flask.ext.httpauth import HTTPBasicAuth
- 
-app = Flask(__name__, static_url_path = "")
+
+app = Flask(__name__, static_url_path="")
 api = Api(app)
 auth = HTTPBasicAuth()
- 
+
+
 @auth.get_password
 def get_password(username):
     if username == 'miguel':
         return 'python'
     return None
- 
+
+
 @auth.error_handler
 def unauthorized():
-    return make_response(jsonify( { 'message': 'Unauthorized access' } ), 403)
-    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
-    
+    # return 403 instead of 401 to prevent browsers from displaying the default
+    # auth dialog
+    return make_response(jsonify({'message': 'Unauthorized access'}), 403)
+
 tasks = [
     {
         'id': 1,
         'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
         'done': False
     },
     {
         'id': 2,
         'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
+        'description': u'Need to find a good Python tutorial on the web',
         'done': False
     }
 ]
- 
+
 task_fields = {
     'title': fields.String,
     'description': fields.String,
@@ -45,17 +47,21 @@ task_fields = {
     'uri': fields.Url('task')
 }
 
+
 class TaskListAPI(Resource):
     decorators = [auth.login_required]
 
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('title', type = str, required = True, help = 'No task title provided', location = 'json')
-        self.reqparse.add_argument('description', type = str, default = "", location = 'json')
+        self.reqparse.add_argument('title', type=str, required=True,
+                                   help='No task title provided',
+                                   location='json')
+        self.reqparse.add_argument('description', type=str, default="",
+                                   location='json')
         super(TaskListAPI, self).__init__()
-        
+
     def get(self):
-        return { 'tasks': map(lambda t: marshal(t, task_fields), tasks) }
+        return {'tasks': [marshal(task, task_fields) for task in tasks]}
 
     def post(self):
         args = self.reqparse.parse_args()
@@ -66,44 +72,47 @@ class TaskListAPI(Resource):
             'done': False
         }
         tasks.append(task)
-        return { 'task': marshal(task, task_fields) }, 201
+        return {'task': marshal(task, task_fields)}, 201
+
 
 class TaskAPI(Resource):
     decorators = [auth.login_required]
-    
+
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('title', type = str, location = 'json')
-        self.reqparse.add_argument('description', type = str, location = 'json')
-        self.reqparse.add_argument('done', type = bool, location = 'json')
+        self.reqparse.add_argument('title', type=str, location='json')
+        self.reqparse.add_argument('description', type=str, location='json')
+        self.reqparse.add_argument('done', type=bool, location='json')
         super(TaskAPI, self).__init__()
 
     def get(self, id):
-        task = filter(lambda t: t['id'] == id, tasks)
+        task = [task for task in tasks if task['id'] == id]
         if len(task) == 0:
             abort(404)
-        return { 'task': marshal(task[0], task_fields) }
-        
+        return {'task': marshal(task[0], task_fields)}
+
     def put(self, id):
-        task = filter(lambda t: t['id'] == id, tasks)
+        task = [task for task in tasks if task['id'] == id]
         if len(task) == 0:
             abort(404)
         task = task[0]
         args = self.reqparse.parse_args()
-        for k, v in args.iteritems():
-            if v != None:
+        for k, v in args.items():
+            if v is not None:
                 task[k] = v
-        return { 'task': marshal(task, task_fields) }
+        return {'task': marshal(task, task_fields)}
 
     def delete(self, id):
-        task = filter(lambda t: t['id'] == id, tasks)
+        task = [task for task in tasks if task['id'] == id]
         if len(task) == 0:
             abort(404)
         tasks.remove(task[0])
-        return { 'result': True }
+        return {'result': True}
 
-api.add_resource(TaskListAPI, '/todo/api/v1.0/tasks', endpoint = 'tasks')
-api.add_resource(TaskAPI, '/todo/api/v1.0/tasks/<int:id>', endpoint = 'task')
-    
+
+api.add_resource(TaskListAPI, '/todo/api/v1.0/tasks', endpoint='tasks')
+api.add_resource(TaskAPI, '/todo/api/v1.0/tasks/<int:id>', endpoint='task')
+
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
--- rest-server.py
+++ rest-server.py
@@ -1,70 +1,82 @@
 #!flask/bin/python
+import six
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask.ext.httpauth import HTTPBasicAuth
- 
-app = Flask(__name__, static_url_path = "")
+
+app = Flask(__name__, static_url_path="")
 auth = HTTPBasicAuth()
- 
+
+
 @auth.get_password
 def get_password(username):
     if username == 'miguel':
         return 'python'
     return None
- 
+
+
 @auth.error_handler
 def unauthorized():
-    return make_response(jsonify( { 'error': 'Unauthorized access' } ), 403)
-    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
-    
+    # return 403 instead of 401 to prevent browsers from displaying the default
+    # auth dialog
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+
 @app.errorhandler(400)
-def not_found(error):
-    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
- 
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify( { 'error': 'Not found' } ), 404)
- 
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 tasks = [
     {
         'id': 1,
         'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
         'done': False
     },
     {
         'id': 2,
         'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
+        'description': u'Need to find a good Python tutorial on the web',
         'done': False
     }
 ]
- 
+
+
 def make_public_task(task):
     new_task = {}
     for field in task:
         if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id = task['id'], _external = True)
+            new_task['uri'] = url_for('get_task', task_id=task['id'],
+                                      _external=True)
         else:
             new_task[field] = task[field]
     return new_task
-    
-@app.route('/todo/api/v1.0/tasks', methods = ['GET'])
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
 @auth.login_required
 def get_tasks():
-    return jsonify( { 'tasks': map(make_public_task, tasks) } )
- 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 @auth.login_required
 def get_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
+    task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
-    return jsonify( { 'task': make_public_task(task[0]) } )
- 
-@app.route('/todo/api/v1.0/tasks', methods = ['POST'])
+    return jsonify({'task': make_public_task(task[0])})
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
 @auth.login_required
 def create_task():
-    if not request.json or not 'title' in request.json:
+    if not request.json or 'title' not in request.json:
         abort(400)
     task = {
         'id': tasks[-1]['id'] + 1,
@@ -73,35 +85,41 @@ def create_task():
         'done': False
     }
     tasks.append(task)
-    return jsonify( { 'task': make_public_task(task) } ), 201
- 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['PUT'])
+    return jsonify({'task': make_public_task(task)}), 201
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
 @auth.login_required
 def update_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
+    task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     if not request.json:
         abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
+    if 'title' in request.json and \
+            not isinstance(request.json['title'], six.string_types):
         abort(400)
-    if 'description' in request.json and type(request.json['description']) is not unicode:
+    if 'description' in request.json and \
+            not isinstance(request.json['description'], six.string_types):
         abort(400)
     if 'done' in request.json and type(request.json['done']) is not bool:
         abort(400)
     task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['description'] = request.json.get('description',
+                                              task[0]['description'])
     task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify( { 'task': make_public_task(task[0]) } )
-    
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['DELETE'])
+    return jsonify({'task': make_public_task(task[0])})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
 @auth.login_required
 def delete_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
+    task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     tasks.remove(task[0])
-    return jsonify( { 'result': True } )
-    
+    return jsonify({'result': True})
+
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
